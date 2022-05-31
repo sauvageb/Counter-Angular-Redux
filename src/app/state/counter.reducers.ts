597ff30a5545337a2c22ab745f7d30e6be733831
sauvageb <sauvageboris.pro@gmail.com>
2022-05-31 14:10:07 +0200

    fix reducer counter

--- src/app/state/counter.reducers.ts
+++ src/app/state/counter.reducers.ts
@@ -1,11 +1,11 @@
 import {createReducer, on} from "@ngrx/store";
-import {increment, reset} from "./counter.actions";
+import {decrement, increment, reset} from "./counter.actions";
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),
-  on(increment, (state) => state - 1),
+  on(decrement, (state) => state - 1),
   on(reset, (state) => 0)
 );
