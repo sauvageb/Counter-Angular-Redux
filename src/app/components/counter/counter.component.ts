a8f28a6be8495192bc52984189a93fbc1609aff3
sauvageb <sauvageboris.pro@gmail.com>
2022-05-31 14:12:15 +0200

    add dispatch and select in counter component

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,13 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Store} from "@ngrx/store";
 import {Observable} from "rxjs";
+import {decrement, increment, reset} from "../../state/counter.actions";
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
 
   numberBoxes$: Observable<number>;
 
@@ -15,8 +16,17 @@ export class CounterComponent implements OnInit {
     this.numberBoxes$ = store.select('count');
   }
 
-  ngOnInit(): void {
+  generate() {
+    this.store.dispatch(increment());
+    console.log('generate');
   }
 
+  remove() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 
 }
