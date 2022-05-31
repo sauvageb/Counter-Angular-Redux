9048ffdb17b54f6be9c2e3ea1205b5a3be7f67a6
sauvageb <sauvageboris.pro@gmail.com>
2022-05-31 12:37:21 +0200

    import reducers in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CounterComponent } from './components/counter/counter.component';
+import {AppComponent} from './app.component';
+import {CounterComponent} from './components/counter/counter.component';
+import {StoreModule} from "@ngrx/store";
+import {counterReducer} from "./state/counter.reducers";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { CounterComponent } from './components/counter/counter.component';
     CounterComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    StoreModule.forRoot({count: counterReducer})],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
